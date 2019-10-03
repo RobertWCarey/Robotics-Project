5b83a4d941bd8a6fd476d1dbb8b686b38a8db087
Rob Carey <rob_carey_1@hotmail.com>
2019-10-04 07:55:03 +1000

    Removed build folder from git tracking

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/will/Documents/RoboticsProject/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/will/Documents/RoboticsProject/catkin_ws/devel;/opt/ros/melodic".split(';') if "/home/will/Documents/RoboticsProject/catkin_ws/devel;/opt/ros/melodic" != "" else []
